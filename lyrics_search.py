ee2a14d378d2766f2f942220458ca7d7d0bbc3f7
Nawafds <nawafds@gmail.com>
2022-11-10 20:14:42 -0500

    working prototype

--- lyrics_search.py
+++ lyrics_search.py
@@ -2,8 +2,8 @@ from flask import Flask, request, render_template
 import json
 import requests
 
-def Lyrics(artist, song):
-    #
+def Lyrics(song):
+    artist = ""
     url = f"http://api.musixmatch.com/ws/1.1/track.search?q_artist={artist}&page_size=1&page=1&s_track_rating=desc&apikey=b653a9863936de49ed3087828dcafe54&q_track={song}"
 
     payload = {}
@@ -13,10 +13,8 @@ def Lyrics(artist, song):
 
     getTrackId = json.loads(response.text)
     getTrackId = int(getTrackId["message"]["body"]["track_list"][0]["track"]["track_id"])
-    #
-
-
-    #
+    
+ 
     url = f"http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id={getTrackId}&apikey=b653a9863936de49ed3087828dcafe54"
 
     payload = {}
@@ -26,20 +24,18 @@ def Lyrics(artist, song):
 
     getLyrics = json.loads(response.text)
     getLyrics = getLyrics["message"]["body"]["lyrics"]["lyrics_body"]
-    # print(getLyrics)
-    #
+   
     return getLyrics
 
 
 app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST']
+)
 def home():
     if request.method == 'POST':
         name = request.form.get('name')
-        username = request.form.get('username')
-        
-        return f"<h1>{Lyrics(username, name)}</h1>"
+
+        return render_template("lyrics_search.html",variable= f"{Lyrics(name)}")
     return render_template("lyrics_search.html")
 
 if __name__ == "__main__":
