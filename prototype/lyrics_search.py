ed79c15c2596795176d504da671866fe334df3cd
Nawafds <nawafds@gmail.com>
2022-11-17 22:22:50 -0500

    getting ai img

--- prototype/lyrics_search.py
+++ prototype/lyrics_search.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, render_template
 import json
 import requests
+import getpass, os
+import io
+import os
+import warnings
+from IPython.display import display
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
 
 def Lyrics(song):
     artist = ""
@@ -42,9 +51,41 @@ def Lyrics(song):
             result += i
     return result
 
+def getImg(keyWords):
+    # NB: host url is not prepended with \"https\" nor does it have a trailing slash.
+    os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
+
+    # To get your API key, visit https://beta.dreamstudio.ai/membership
+    os.environ['STABILITY_KEY'] = getpass.getpass('Enter your API Key')
+
+    stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'], 
+    verbose=True,)
+
+
+    # the object returned is a python generator
+    answers = stability_api.generate(
+    prompt= keyWords,
+    seed=34567, # if provided, specifying a random seed makes results deterministic
+    steps=30,) # defaults to 50 if not specified
+
+
+    # iterating over the generator produces the api response
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                    "Your request activated the API's safety filters and could not be processed."
+                    "Please modify the prompt and try again.")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                display(img)    
+
+
+
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST']
-)
+@app.route('/', methods=['GET', 'POST'])
+
 def home():
     if request.method == 'POST':
         name = request.form.get('name')
@@ -56,6 +97,6 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-
+getImg("camila cabello havana")
 
 
