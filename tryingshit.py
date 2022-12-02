2c2467092790c32e96746a6f56a064c3c24de9ee
Nawafds <nawafds@gmail.com>
2022-12-02 12:56:06 -0500

    can generate photos now and create a different folder for prototype

---
+++ tryingshit.py
@@ -0,0 +1,7 @@
+import replicate
+model = replicate.models.get("stability-ai/stable-diffusion")
+version = model.versions.get("27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478")
+output = version.predict(prompt="havana")
+
+
+print(output)
