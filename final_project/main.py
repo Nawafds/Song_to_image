7e2cbcde4ea50d683113b409ca26c3cdd47c88d6
Nawafds <nawafds@gmail.com>
2022-12-04 17:31:16 -0500

    log in works profile page works and logout works

--- final_project/main.py
+++ final_project/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, session
 import json
 import requests
 import getpass, os
@@ -11,7 +11,17 @@ from IPython.display import display
 from PIL import Image
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
-import db
+import mysql.connector
+from mysql.connector import Error
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="12345678",
+    database="SongToImgDB"
+)
+
+mycursor = mydb.cursor()
 
 
 def Lyrics(song):
@@ -25,7 +35,7 @@ def Lyrics(song):
 
     getTrackId = json.loads(response.text)
 
-    if(int(getTrackId["message"]["header"]["available"])== 0):
+    if (int(getTrackId["message"]["header"]["available"]) == 0):
         return "No lyrics found"
 
     getTrackId = int(getTrackId["message"]["body"]["track_list"][0]["track"]["track_id"])
@@ -35,7 +45,6 @@ def Lyrics(song):
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload)
-
 
     getLyrics = json.loads(response.text)
 
@@ -54,6 +63,7 @@ def Lyrics(song):
             result += i
     return result
 
+
 def getImg(keyWords, name):
     # NB: host url is not prepended with \"https\" nor does it have a trailing slash.
     os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
@@ -62,17 +72,16 @@ def getImg(keyWords, name):
     os.environ['STABILITY_KEY'] = 'sk-ZW2FwVJtzNT6wlBdQEw7vAcpVLHXXzwgIeGwA3A5aaOvvUt1'
 
     stability_api = client.StabilityInference(
-    key=os.environ['STABILITY_KEY'],
-    verbose=True,)
-
+        key=os.environ['STABILITY_KEY'],
+        verbose=True, )
 
     # the object returned is a python generator
     answers = stability_api.generate(
-    prompt= keyWords,
-    seed=34567, # if provided, specifying a random seed makes results deterministic
-    steps=30,) # defaults to 50 if not specified
+        prompt=keyWords,
+        seed=34567,  # if provided, specifying a random seed makes results deterministic
+        steps=30, )  # defaults to 50 if not specified
 
-    #iterating over the generator produces the api response
+    # iterating over the generator produces the api response
     for resp in answers:
         for artifact in resp.artifacts:
             if artifact.finish_reason == generation.FILTER:
@@ -80,32 +89,85 @@ def getImg(keyWords, name):
                     "Your request activated the API's safety filters and could not be processed."
                     "Please modify the prompt and try again.")
             if artifact.type == generation.ARTIFACT_IMAGE:
-                data = Image.open(io.BytesIO(artifact.binary))
-                data = data.save("photos/" + name + ".png")
+                return Image.open(io.BytesIO(artifact.binary))
 
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
 
-def home():
+app.secret_key = "12345678"
+
+
+@app.route('/img', methods=['GET', 'POST'])
+def find_img():
     if request.method == 'POST':
 
-        name = request.form.get('name1')
+        name = request.form.get('name')
 
         song_lyrics = Lyrics(name)
+        if song_lyrics == "No lyrics found":
+            return render_template("lyrics_search.html", msg=f"{song_lyrics}")
+        else:
+            im = getImg(song_lyrics, name)
+            data = io.BytesIO()
+            im.save(data, "JPEG")
+            encoded_img_data = base64.b64encode(data.getvalue())
+            # return render_template("lyrics_search.html",msg = f"{song_lyrics}", img_data=encoded_img_data.decode('utf-8'), song_name = name )
 
-        getImg(song_lyrics, name)
+            return render_template("home.html", msg=f"{song_lyrics}", img_data=encoded_img_data.decode('utf-8'))
+    return render_template("home.html")
 
-        db.addUserImg(counter,name,1, db.convertToBinaryData("photos/" + name + ".png"))
-        im = Image.open("photos/" + name + ".png")
-        data = io.BytesIO()
-        im.save(data, "JPEG")
-        encoded_img_data = base64.b64encode(data.getvalue())
 
-        return render_template("lyrics_search.html",variable = f"{song_lyrics}", img_data=encoded_img_data.decode('utf-8'), song_name = name )
-    return render_template("lyrics_search.html")
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    msg = ""
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        mycursor.execute("SELECT * FROM Users WHERE Username = %s AND password = %s", (username, password))
+        account = mycursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['username'] = username
+            session['password'] = password
+            msg = 'Logged in successfully!'
+            return redirect('/home')
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+
+        print(username, password)
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('loggedin', None)
+    session.pop('username', None)
+    return redirect('/')
+
+
+@app.route('/home')
+def home():
+    if 'loggedin' in session:
+        return redirect('/img')
+    return redirect("/login")
+
+
+@app.route('/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        mycursor.execute("SELECT * FROM Users WHERE Username = %s AND password = %s", (session["username"], session['password']))
+        account = mycursor.fetchone()
+        print(account[0])
+
+        # Show the profile page with account info
+        return render_template('profile.html', username=account[0], password = account[1], email = account[2])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
--- final_project/tryingshit.py
+++ final_project/tryingshit.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 from mysql.connector import Error
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("l")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
